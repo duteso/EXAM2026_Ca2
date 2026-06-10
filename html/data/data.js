b3d64f52ffe07e5a0777bbb4c2bcd64cc7e9fbc7
duteso <potatoguy1511@gmail.com>
2026-06-10 16:33:58 +0700

    Hoan thien co ban cau 1

--- html/data/data.js
+++ html/data/data.js
@@ -2,35 +2,35 @@ const schools = [
   {
     id: 1,
     name: "Trường THPT Nguyễn Huệ",
-    district: "Thủy Nguyên",
+    district: "Thủy Phòng GD&ĐT TP. Thủy Lợi",
     status: "Hoàn thành",
-    description: "Đã hoàn thành chuyển đổi số."
+    description: "Hoàn thành chuyển đổi số toàn diện; Hệ thống quản lý học tập LMS, phần mềm quản lý hành chính, chữ ký số và hạ tầng mạng Wifi Campus"
   },
   {
     id: 2,
     name: "Trường THCS Lê Quý Đôn",
-    district: "Hồng Bàng",
+    district: "Thủy Phòng GD&ĐT H.Thủy Nguyên",
     status: "Đang triển khai",
-    description: "Đang triển khai hệ thống quản lý."
+    description: "Đang triển khai phần mềm học bá điện tử và hệ thống camera giám sát lớp học. Đã hoàn thành 60%."
   },
   {
     id: 3,
     name: "Trường THPT Trần Phú",
-    district: "Lê Chân",
+    district: "Phòng GD&ĐT Q. Hồng Bàng",
     status: "Chưa bắt đầu",
-    description: "Chưa thực hiện dự án."
+    description: "Chưa bắt đầu chuyển đổi số. Đang chờ kinh phí và kế hoạch từ Sở Giáo Dục"
   },
   {
     id: 4,
-    name: "Trường THCS Nguyễn Trãi",
-    district: "Hải An",
+    name: "Trường THCS Nguyễn Trí Phương",
+    district: "Thủy Phòng GD&ĐT TP. Thủy Lợi",
     status: "Đang triển khai",
     description: "Đang nâng cấp hạ tầng CNTT."
   },
   {
     id: 5,
-    name: "Trường THPT Ngô Quyền",
-    district: "Kiến An",
+    name: "Trường THPT Giáp Hải",
+    district: "Bắc Giang",
     status: "Hoàn thành",
     description: "Đã hoàn thành hệ thống số hóa."
   }
